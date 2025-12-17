e248249f9474fc3fb27ed88c658fdaf74fb7ca1b
reda <9narutouzumaki29999@gmail.com>
2025-12-17 23:07:02 +0100

    add the page statique of all page

--- src/BACK/API/signin.php
+++ src/BACK/API/signin.php
@@ -22,29 +22,56 @@
 
     if($role == "coach") {
         $BioCoach = $_POST["BioCoach"] ; 
-        $experiencecoach = $_POST["experiencecoach"] ; 
+        $experiencecoach = $_POST["experiencecoach"] ;
+
+        // Define upload directories
+        $profilePhotoDir = __DIR__ . "/../../FRONT/IMG/PROFILESPHOTO/";
+        $certificatePhotoDir = __DIR__ . "/../../FRONT/IMG/CERTIFICATEPHOTO/";
+
+        // Create directories if they don't exist
+        if (!file_exists($profilePhotoDir)) {
+            mkdir($profilePhotoDir, 0777, true);
+        }
+        if (!file_exists($certificatePhotoDir)) {
+            mkdir($certificatePhotoDir, 0777, true);
+        }
 
         $profilePhotoPath = '';
         if($_FILES["profilePhoto"]["error"] == 0 ){
-            $nameProfilePhoto =  $_FILES["profilePhoto"]["name"] ; 
-            $uploadDirectionProfile = "../../FRONT/IMG/PROFILESPHOTO/" . $nameProfilePhoto ;
-            move_uploaded_file( $_FILES["profilePhoto"]["tmp_name"] , $uploadDirectionProfile) ;
-            chmod($uploadDirectionProfile, 0777);
-            $profilePhotoPath = '../IMG/PROFILESPHOTO/' . $nameProfilePhoto ; 
+            $originalName = $_FILES["profilePhoto"]["name"] ; 
+            $fileExtension = pathinfo($originalName, PATHINFO_EXTENSION);
+            // Sanitize filename: remove spaces and special characters, use timestamp for uniqueness
+            $sanitizedName = preg_replace('/[^a-zA-Z0-9_-]/', '_', pathinfo($originalName, PATHINFO_FILENAME));
+            $nameProfilePhoto = $sanitizedName . '_' . time() . '_' . $selectUser . '.' . $fileExtension;
+            $uploadDirectionProfile = $profilePhotoDir . $nameProfilePhoto ;
+            
+            if (move_uploaded_file($_FILES["profilePhoto"]["tmp_name"], $uploadDirectionProfile)) {
+                chmod($uploadDirectionProfile, 0644);
+                $profilePhotoPath = '../IMG/PROFILESPHOTO/' . $nameProfilePhoto ; 
+            } else {
+                error_log("Failed to upload profile photo: " . $_FILES["profilePhoto"]["error"]);
+            }
         }
 
         $certificatePhotoPath = '';
         if($_FILES["certificate"]["error"] == 0 ){
-            $nameCertifPhoto = $_FILES["certificate"]["name"] ; 
-            $uploadDirectionCertif = "../../FRONT/IMG/CERTIFICATEPHOTO/" . $nameCertifPhoto ;
-            move_uploaded_file($_FILES["certificate"]["tmp_name"] , $uploadDirectionCertif) ; 
-            chmod($uploadDirectionCertif, 0777);
-            $certificatePhotoPath = '../IMG/CERTIFICATEPHOTO/' . $nameCertifPhoto ; 
+            $originalCertifName = $_FILES["certificate"]["name"] ; 
+            $fileExtension = pathinfo($originalCertifName, PATHINFO_EXTENSION);
+            // Sanitize filename: remove spaces and special characters, use timestamp for uniqueness
+            $sanitizedName = preg_replace('/[^a-zA-Z0-9_-]/', '_', pathinfo($originalCertifName, PATHINFO_FILENAME));
+            $nameCertifPhoto = $sanitizedName . '_' . time() . '_' . $selectUser . '.' . $fileExtension;
+            $uploadDirectionCertif = $certificatePhotoDir . $nameCertifPhoto ;
+            
+            if (move_uploaded_file($_FILES["certificate"]["tmp_name"], $uploadDirectionCertif)) {
+                chmod($uploadDirectionCertif, 0644);
+                $certificatePhotoPath = '../IMG/CERTIFICATEPHOTO/' . $nameCertifPhoto ; 
+            } else {
+                error_log("Failed to upload certificate photo: " . $_FILES["certificate"]["error"]);
+            }
         }
 
         $insertcoashprepare = $connect -> prepare('INSERT INTO coach_profile (coach_id , bio , experience_year , certification , photo) VALUE ( ? , ? , ? , ? , ?)') ; 
         $insertcoashprepare -> execute([$selectUser , $BioCoach , $experiencecoach , $profilePhotoPath , $certificatePhotoPath]) ; 
 
     }
-
-    echo "T9DA LGHARAD" ; 
+    echo "T9DA LGHARAD" ;
